10e6a03104b7bdcb3443bcf077e8b2a07dba510d
Geovanny Lema <geova.lem@hotmail.com>
2024-07-12 23:22:46 -0500

    add ejercicio 9

---
+++ ejercicio-9.js
@@ -0,0 +1,9 @@
+//Hacer un pseudocódigo que sólo nos permite introducir S o N.
+/* 
+              Estudio Previo
+    El teclado puede estar en minusculas; en esa caso, debemos pasarlo a mayusculas.
+    La expresion: 
+                    Convertir_mayusculas(variable)
+    el contenido de la variable, que esta entre parentesis, la convierte a mayusculas. 
+
+ */ 
